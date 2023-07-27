41660785f4c956336a994163a0eacaa5817f6870
Li Zhu <lizhu129@gmail.com>
2023-07-27 18:47:25 +0200

    add all the validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_141653) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_164628) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_141653) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "rating"
+    t.integer "rating"
     t.text "content"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
