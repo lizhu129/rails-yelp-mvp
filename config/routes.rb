e659e71adea045e5ee88212f64b785653d2dfb55
Li Zhu <lizhu129@gmail.com>
2023-07-28 15:24:49 +0200

    add all methods and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :new, :create, :show] do
-    resources :reviews, only: [:index, :new, :create]
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[index new create]
   end
 end
